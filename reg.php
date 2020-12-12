b8b6eb0684f36356fddc08a7a02a84f490edbb7b
Aman sharma <52895888+Aman-Sharmaa@users.noreply.github.com>
2020-12-12 03:47:27 -0800

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Title</title>
+	<link rel="stylesheet" type="text/css" href="new-style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Keania+One&family=Monoton&display=swap" rel="stylesheet"> 
+</head>
+<body>
+
+	<div class="back">
+      <a href="index.html"><p>Back / HomePage </p></a>
+	</div>
+
+   <div class="common-login">
+   	 <h4>Create new account </h4>
+
+   
+    <input type="text" name="name" placeholder="Name">
+    <input type="email" name="email" placeholder="Email id">
+    <input type="number" name="c_num" placeholder="Number">
+   	<input type="text" name="username" placeholder="Username">
+    <input type="text" name="password" placeholder="Password">
+    <button>Create new account</button>
+    <div class="reg">
+           <p><a href="login.php">Login</a></p>
+           <p>Back</p>
+    </div>
+
+   </div>
+
+</body>
